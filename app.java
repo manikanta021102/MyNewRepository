ac8cf08a40920d7a2def31739c115fe51e0143ca
manikanta021102 <manikanta.kandula01@gmail.com>
2025-11-14 04:29:06 +0530

    Update app.java

--- app.java
+++ app.java
@@ -2,8 +2,8 @@ class Main {
 
   public static void main(String[] args) {
     
-    int first = 10;
-    int second = 20;
+    int first = 200;
+    int second = 203;
 
     // add two numbers
     int sum = first + second;
@@ -19,6 +19,7 @@ class Main {
 }
 
 You are awesome aws 
+
 
 
 
